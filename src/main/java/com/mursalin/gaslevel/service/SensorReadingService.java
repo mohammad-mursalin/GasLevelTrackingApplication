fcd3939c61ab5bd9ba95ea66e1a98fedf6d5fa1f
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-27 23:40:30 +0600

    all business type logic is moved to service layer

--- src/main/java/com/mursalin/gaslevel/service/SensorReadingService.java
+++ src/main/java/com/mursalin/gaslevel/service/SensorReadingService.java
@@ -1,14 +1,17 @@
-//package com.mursalin.gaslevel.service;
-//
-//import com.mursalin.gaslevel.model.SensorReading;
-//
-//import java.util.List;
-//
-//public interface SensorReadingService {
-//
-//    SensorReading saveSensorReading(SensorReading sensorReading);
-//
-//    List<SensorReading> getAllSensorReadings();
-//
-//    SensorReading getLatestSensorReading();
-//}
+package com.mursalin.gaslevel.service;
+
+import com.mursalin.gaslevel.model.SensorReading;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SensorReadingService {
+
+    SensorReading saveSensorReading(SensorReading sensorReading);
+
+    List<SensorReading> getAllSensorReadings();
+
+    double getLatestSensorReading();
+
+    void receivedSensorData(Map<String, Object> payload);
+}
--- src/main/java/com/mursalin/gaslevel/service/impl/SensorReadingServiceImpl.java
+++ src/main/java/com/mursalin/gaslevel/service/impl/SensorReadingServiceImpl.java
@@ -1,35 +1,64 @@
-//package com.mursalin.gaslevel.service.impl;
-//
-//import com.mursalin.gaslevel.model.SensorReading;
-//import com.mursalin.gaslevel.repository.SensorReadingRepository;
-//import com.mursalin.gaslevel.service.SensorReadingService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class SensorReadingServiceImpl implements SensorReadingService {
-//
-//    private final SensorReadingRepository sensorReadingRepository;
-//
-//    @Autowired
-//    public SensorReadingServiceImpl(SensorReadingRepository sensorReadingRepository) {
-//        this.sensorReadingRepository = sensorReadingRepository;
-//    }
-//
-//    @Override
-//    public SensorReading saveSensorReading(SensorReading sensorReading) {
-//        return sensorReadingRepository.save(sensorReading);
-//    }
-//
-//    @Override
-//    public List<SensorReading> getAllSensorReadings() {
-//        return sensorReadingRepository.findAll();
-//    }
-//
-//    @Override
-//    public SensorReading getLatestSensorReading() {
-//        return sensorReadingRepository.findFirstByOrderByTimestampDesc();
-//    }
-//}
+package com.mursalin.gaslevel.service.impl;
+
+import com.mursalin.gaslevel.exception.CustomException;
+import com.mursalin.gaslevel.model.SensorReading;
+import com.mursalin.gaslevel.repository.SensorReadingRepository;
+import com.mursalin.gaslevel.service.SensorReadingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SensorReadingServiceImpl implements SensorReadingService {
+
+    private final SensorReadingRepository sensorReadingRepository;
+    private double sensorValue = -1;
+    private String location = "";
+
+    @Autowired
+    public SensorReadingServiceImpl(SensorReadingRepository sensorReadingRepository) {
+        this.sensorReadingRepository = sensorReadingRepository;
+    }
+
+    @Override
+    public void receivedSensorData(Map<String, Object> payload) {
+
+        sensorValue = (double) payload.get("sensorValue");
+
+        sensorReadingRepository.save(new SensorReading(location, sensorValue, LocalDateTime.now()));
+
+        System.out.println("Received Gas Concentration: " + sensorValue + " ppm");
+
+    }
+
+    @Override
+    public SensorReading saveSensorReading(SensorReading sensorReading) {
+
+        sensorReading.setTimestamp(LocalDateTime.now());
+        updateLocation(sensorReading.getLocation());
+        return sensorReadingRepository.save(sensorReading);
+    }
+
+    @Override
+    public List<SensorReading> getAllSensorReadings() {
+        return sensorReadingRepository.findAll();
+    }
+
+    @Override
+    public double getLatestSensorReading() {
+
+        if(sensorValue < 0 ) {
+            throw new CustomException("sensor is not sending correct value");
+        }
+        return sensorValue;
+    }
+
+
+
+    private void updateLocation(String location) {
+        this.location = location;
+    }
+}
