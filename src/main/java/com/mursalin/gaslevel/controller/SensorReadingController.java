7e2ab81d36c09a47fd4797b479c46d456e9dd63a
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-27 23:40:16 +0600

    all business type logic is moved to service layer

--- src/main/java/com/mursalin/gaslevel/controller/SensorReadingController.java
+++ src/main/java/com/mursalin/gaslevel/controller/SensorReadingController.java
@@ -1,10 +1,12 @@
 package com.mursalin.gaslevel.controller;
 
-//import com.mursalin.gaslevel.model.SensorReading;
-//import com.mursalin.gaslevel.service.SensorReadingService;
+import com.mursalin.gaslevel.model.SensorReading;
+import com.mursalin.gaslevel.service.SensorReadingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -12,62 +14,38 @@ import java.util.Map;
 @CrossOrigin(value = "http://localhost:5173")
 public class SensorReadingController {
 
-//    private final SensorReadingService sensorReadingService;
-//
-//    @Autowired
-//    public SensorReadingController(SensorReadingService sensorReadingService) {
-//        this.sensorReadingService = sensorReadingService;
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<String> saveSensorReading(@RequestBody SensorReading sensorReading) {
-//        sensorReading.setTimestamp(LocalDateTime.now());
-//        sensorReadingService.saveSensorReading(sensorReading);
-//        return ResponseEntity.ok("Sensor reading saved successfully.");
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<List<SensorReading>> getAllSensorReadings() {
-//        return ResponseEntity.ok(sensorReadingService.getAllSensorReadings());
-//    }
-//
-//    @GetMapping("/latest")
-//    public ResponseEntity<SensorReading> getLatestSensorReading() {
-//        SensorReading latestReading = sensorReadingService.getLatestSensorReading();
-//        if (latestReading != null) {
-//            return ResponseEntity.ok(latestReading);
-//        } else {
-//            return ResponseEntity.noContent().build();
-//        }
-//    }
+    private final SensorReadingService sensorReadingService;
 
-    int reading;
+    @Autowired
+    public SensorReadingController(SensorReadingService sensorReadingService) {
+        this.sensorReadingService = sensorReadingService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SensorReading>> getAllSensorReadings() {
+        return ResponseEntity.ok(sensorReadingService.getAllSensorReadings());
+    }
 
     @GetMapping("/data")
     public ResponseEntity<String> checkBackendStatus() {
         return ResponseEntity.ok("Backend is up and running!");
     }
 
-//    @PostMapping("/data")
-//    public ResponseEntity<String> receiveSensorData(@RequestParam int sensorValue) {
-//        System.out.println("Received sensor data: " + sensorValue);
-//        reading = sensorValue;
-//        return ResponseEntity.ok("Data received successfully");
-//    }
-
-    double sensorValue;
 
     @PostMapping("/data")
     public ResponseEntity<?> receiveSensorDataNew(@RequestBody Map<String, Object> payload) {
-        sensorValue = (Double) payload.get("sensorValue");
-        System.out.println("Received Gas Concentration: " + sensorValue + " ppm");
+
+        sensorReadingService.receivedSensorData(payload);
 
         return ResponseEntity.ok("Data received");
     }
 
-    @GetMapping("/latest")
-    public ResponseEntity<Double> getLatestSensorReading() {
-        double latestReading = sensorValue;
+    @PostMapping("/latest")
+    public ResponseEntity<Double> getLatestSensorReading(@RequestBody SensorReading sensorReading) {
+
+        sensorReadingService.saveSensorReading(sensorReading);
+
+        double latestReading = sensorReadingService.getLatestSensorReading();
 
         if (latestReading != 0) {
             return ResponseEntity.ok(latestReading);
@@ -75,6 +53,5 @@ public class SensorReadingController {
             return ResponseEntity.noContent().build();
         }
     }
-
 
 }
